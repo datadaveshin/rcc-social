3fb15776b73b29e2cd3458db7cd5de2fcf27e585
David Shin <davidshin444@gmail.com>
2019-03-19 18:21:46 -0700

    Add an association to get followers - reverse subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
 
   has_many :leaders, through: :subscriptions
 
+  has_many :reverse_subscriptions, foreign_key: :leader_id,
+                                   class_name: 'Subscription',
+                                   dependent: :destroy
+
+  has_many :followers, through: :reverse_subscriptions
+
   def following?(leader)
     leaders.include? leader
   end
